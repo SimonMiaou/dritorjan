4ebc32bde2d5be3c813a6ad616e3b86a8598e4bb
Simon Lacroix <simon@miaou.be>
2015-06-05 23:58:02 +0200

    Web improvments

--- lib/webserver.rb
+++ lib/webserver.rb
@@ -1,7 +1,17 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'slim'
+
+require 'entry'
 
 class Webserver < Sinatra::Base
+  register Sinatra::AssetPack
+  set :root, File.dirname(__FILE__)
+
+  assets do
+  end
+
   get '/' do
-    erb :index
+    slim :index
   end
 end
