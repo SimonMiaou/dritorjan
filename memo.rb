cb81511a56c7ddc0e56a0477ec217119a24a5522
Simon Lacroix <simon@miaou.be>
2017-03-30 01:15:57 +0200

    Install Rubocop (#2)

--- memo.rb
+++
@@ -1,4 +0,0 @@
-# http://stackoverflow.com/questions/4508692/get-available-diskspace-in-ruby
-require 'sys/filesystem'
-stat = Sys::Filesystem.stat("/")
-mb_available = stat.block_size * stat.blocks_available / 1024 / 1024
