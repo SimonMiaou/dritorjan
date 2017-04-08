42957e6e9b0c1c62042156784664e8b876073343
Simon Lacroix <simon@miaou.be>
2017-04-08 21:42:31 +0200

    Use test settings

--- init.rb
+++ init.rb
@@ -2,4 +2,4 @@ ROOT_PATH = File.dirname(__FILE__)
 $LOAD_PATH.unshift(ROOT_PATH)
 
 require 'config'
-Config.load_and_set_settings(Config.setting_files('./config', 'development'))
+Config.load_and_set_settings(Config.setting_files('./config', ENV['DRITORJAN_ENV'] || 'development'))
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,6 @@ require 'rr'
 
 require 'webmock/minitest'
 WebMock.enable!
+
+Config.load_and_set_settings(Config.setting_files('./config', 'test'))
+Dir.mkdir('./tmp') unless Dir.exist?('./tmp')
