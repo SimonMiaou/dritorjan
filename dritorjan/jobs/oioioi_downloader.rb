d25d967ed5d890a7c486dc84abadd659fe039cdd
Simon Lacroix <simon@miaou.be>
2017-04-09 23:50:22 +0200

    Finalize OioioiDownloader

--- dritorjan/jobs/oioioi_downloader.rb
+++ dritorjan/jobs/oioioi_downloader.rb
@@ -9,45 +9,38 @@ module Dritorjan
       include Sidekiq::Worker
 
       URL = 'http://oioioi.ru/mp3/skinhead/moonstomp.html'.freeze
-      REGEX = %r{/\Ahttp:\/\/www\.skinhead\.ru\/mp3\/(.*\.mp3)\z/i}
+      REGEX = %r{\Ahttp://www\.skinhead\.ru/mp3/(.*\.mp3)\z}i
 
       def perform
-        ensure_directory_exist
+        ensure_directory_exist(Settings.oioioi.directory)
 
-        page = Nokogiri::HTML(Net::HTTP.get(URI.parse(URL)))
-        links = page.css('a')
-        hrefs = links.map { |l| l['href'] }.compact
-        mp3_hrefs = hrefs.select { |url| url.match REGEX }
+        mp3_urls.each do |mp3_url|
+          file_path = "#{Settings.oioioi.directory}/#{mp3_url.match(REGEX)[1]}"
+          next if File.exist?(file_path)
 
-        mp3_hrefs.each do |href|
-          path = href.match(regex)[1]
-          save(href, folder + path)
+          ensure_directory_exist(File.dirname(file_path))
+          download_file(mp3_url, file_path)
         end
-      rescue Net::OpenTimeout
       end
 
       private
 
-      def ensure_directory_exist
-        Dir.mkdir(Settings.oioioi.directory) unless Dir.exist?(Settings.oioioi.directory)
+      def ensure_directory_exist(dir)
+        Dir.mkdir(dir) unless Dir.exist?(dir)
       end
 
-      def save(url, path)
-        if File.exist?(path)
-          puts "Already saved: #{url}"
-          return
+      def mp3_urls
+        raw_html = Net::HTTP.get(URI.parse(URL))
+        web_page = Nokogiri::HTML(raw_html)
+        links = web_page.css('a').map { |l| l['href'] }
+        links.compact.uniq.select { |url| url.match REGEX }
+      end
+
+      def download_file(url, file_path)
+        file_body = Net::HTTP.get(URI.parse(URI.escape(url)))
+        File.open(file_path, 'wb') do |file|
+          file << file_body
         end
-
-        puts "Downloading: #{url}"
-
-        dir = File.dirname(path)
-        Dir.mkdir(dir) unless Dir.exist?(dir)
-
-        File.open(path, 'wb') do |file|
-          file << Net::HTTP.get(URI.parse(url))
-        end
-
-        puts "Saved in: #{path}"
       end
     end
   end
