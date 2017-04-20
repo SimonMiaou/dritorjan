6f26604060b22f3e2fad77c30fe1cb77accdd356
Simon Lacroix <simon@miaou.be>
2017-04-21 01:36:46 +0200

    Make code nicer - part 2

--- test/jobs/test_directory_size_updater.rb
+++ test/jobs/test_directory_size_updater.rb
@@ -13,8 +13,7 @@ module Dritorjan
 
       def test_update_size_of_the_directory_with_the_sum_of_the_entries_size
         root_path = File.realpath('.')
-        dir = create(:dir_entry, path: "#{root_path}/tmp",
-                                 size: 0)
+        dir = create(:dir_entry, path: "#{root_path}/tmp", size: 0)
         file_foo = create(:file_entry, path: "#{root_path}/tmp/foo.txt")
         file_bar = create(:file_entry, path: "#{root_path}/tmp/bar.txt")
 
