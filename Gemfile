6d010d424893a69664344d61eccf064b1426a83c
Simon Lacroix <simon@miaou.be>
2017-04-24 23:37:48 +0200

    Improve web interface (#21)

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby '2.4.1'
 
 gem 'activerecord'
+gem 'addressable'
 gem 'bundler'
 gem 'config'
 gem 'nokogiri'
--- dritorjan/web_app.rb
+++ dritorjan/web_app.rb
@@ -2,10 +2,41 @@ require 'dritorjan/models/entry'
 require 'sinatra'
 require 'slim'
 
+require 'addressable'
+
 module Dritorjan
   class WebApp < Sinatra::Base
+    helpers do
+      def breadcrumb(entry)
+        b = ''
+
+        while entry.path != '/'
+          link = "<a href=\"#{Addressable::URI.encode(entry.path)}\">#{entry.basename}</a>"
+          b = "/#{link}#{b}"
+          entry = entry.parent
+        end
+
+        b
+      end
+
+      def format_size(size)
+        if size > 1_000_000_000
+          size = (size.to_f / 1_000_000_000).round(2)
+          "#{size} Go"
+        elsif size > 1_000_000
+          size = (size.to_f / 1_000_000).round(2)
+          "#{size} Mo"
+        elsif size > 1_000
+          size = (size.to_f / 1_000).round(2)
+          "#{size} Ko"
+        else
+          "#{size} o"
+        end
+      end
+    end
+
     get(/\A(.*)\z/) do
-      @entry = Models::Entry.find_by path: params['captures'].first
+      @entry = Models::Entry.find params['captures'].first
       slim :index
     end
   end
