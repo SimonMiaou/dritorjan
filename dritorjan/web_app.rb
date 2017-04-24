897e6d051af7103dda3b8f350acc5fa4a4d3f37d
Simon Lacroix <simon@miaou.be>
2017-04-24 23:25:18 +0200

    Pretty sizes

--- dritorjan/web_app.rb
+++ dritorjan/web_app.rb
@@ -6,6 +6,23 @@ require 'addressable'
 
 module Dritorjan
   class WebApp < Sinatra::Base
+    helpers do
+      def format_size(size)
+        if size > 1_000_000_000
+          size = (size.to_f / 1_000_000_000).round(2)
+          "#{size} Go"
+        elsif size > 1_000_000
+          size = (size.to_f / 1_000_000).round(2)
+          "#{size} Mo"
+        elsif size > 1_000
+          size = (size.to_f / 1_000).round(2)
+          "#{size} Ko"
+        else
+          "#{size} o"
+        end
+      end
+    end
+
     get(/\A(.*)\z/) do
       @entry = Models::Entry.find params['captures'].first
       slim :index
