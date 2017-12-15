7fd35a223f1a89485240bec7941c06d8e9acfda7
Simon Lacroix <simon@miaou.be>
2017-12-15 17:28:08 +0100

    Don't enforce airbrake

--- dritorjan/initializers/airbrake.rb
+++ dritorjan/initializers/airbrake.rb
@@ -1,10 +1,12 @@
 require 'airbrake'
 require 'dritorjan'
 
-Airbrake.configure do |c|
-  c.project_id = Settings.airbrake.project_id
-  c.project_key = Settings.airbrake.project_key
+unless Settings.airbrake.nil?
+  Airbrake.configure do |c|
+    c.project_id = Settings.airbrake.project_id
+    c.project_key = Settings.airbrake.project_key
 
-  c.environment = Dritorjan.env
-  c.ignore_environments = Settings.airbrake.ignore_environments || %w[development test]
+    c.environment = Dritorjan.env
+    c.ignore_environments = Settings.airbrake.ignore_environments || %w[development test]
+  end
 end
